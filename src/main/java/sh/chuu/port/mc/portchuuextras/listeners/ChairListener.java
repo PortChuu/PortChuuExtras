61235153cbd13b172988347c4fedaf88eec84fa2
Simon Chuu <simonorj@outlook.com>
2020-03-03 02:13:02 -0500

    ChairListener: Fix corner stair eject location + Add chair entry/eject check

--- src/main/java/sh/chuu/port/mc/portchuuextras/listeners/ChairListener.java
+++ src/main/java/sh/chuu/port/mc/portchuuextras/listeners/ChairListener.java
@@ -3,6 +3,7 @@ package sh.chuu.port.mc.portchuuextras.listeners;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Bisected;
 import org.bukkit.block.data.type.Stairs;
 import org.bukkit.entity.ArmorStand;
@@ -25,7 +26,7 @@ public class ChairListener implements Listener {
     private static final String SIT_PERM = "portchuuextras.interact.sit";
     private final PortChuuExtras plugin = PortChuuExtras.getInstance();
     private final Map<ArmorStand, Block> mounted = new LinkedHashMap<>();
-    private final Map<Entity, Location> postDismountTeleport = new LinkedHashMap<>();
+    private final Map<Player, Location> postDismountTeleport = new LinkedHashMap<>();
 
     public void onDisable() {
         mounted.entrySet().removeIf(as -> {
@@ -40,15 +41,25 @@ public class ChairListener implements Listener {
         Player p = ev.getPlayer();
         if (!p.hasPermission(SIT_PERM)
                 || p.isSneaking()
+                || p.isSprinting()
                 || ev.getAction() != Action.RIGHT_CLICK_BLOCK
                 || ev.isBlockInHand()
+                || ev.getMaterial().isEdible()
         ) return;
 
         Block b = ev.getClickedBlock();
-        if (b != null && b.getBlockData() instanceof Stairs) {
-            Stairs s = (Stairs) b.getBlockData();
-            if (s.getHalf() == Bisected.Half.BOTTOM) {
-                chairMount(ev.getPlayer(), b);
+        if (b != null) {
+            // Distance check - deny if too far distance or if y level too low
+            Location bLoc = b.getLocation();
+            Location pLoc = p.getLocation();
+            if (bLoc.getY() > pLoc.getY() + 0.5 || bLoc.distanceSquared(pLoc) > 4d)
+                return;
+
+            if (b.getBlockData() instanceof Stairs) {
+                Stairs s = (Stairs) b.getBlockData();
+                if (canSit(b, s, ev.getBlockFace())) {
+                    chairMount(ev.getPlayer(), b, s);
+                }
             }
         }
     }
@@ -72,9 +83,7 @@ public class ChairListener implements Listener {
         Location loc = postDismountTeleport.remove(p);
         if (loc != null) {
             ev.setTo(loc);
-            Bukkit.getScheduler().runTaskLater(plugin, () -> {
-                p.teleport(loc, PlayerTeleportEvent.TeleportCause.UNKNOWN);
-            }, 2);
+            Bukkit.getScheduler().runTaskLater(plugin, () -> p.teleport(loc, PlayerTeleportEvent.TeleportCause.PLUGIN), 2);
         }
     }
 
@@ -84,9 +93,65 @@ public class ChairListener implements Listener {
             ev.getDrops().clear();
     }
 
-    private void chairMount(Player p, Block b) {
-        Stairs s = (Stairs) b.getBlockData();
+    private boolean canSit(Block b, Stairs s, BlockFace click) {
+        if (s.getHalf() != Bisected.Half.BOTTOM
+                || !b.getRelative(BlockFace.UP).isPassable()
+                || s.getFacing() == click
+        )
+            return false;
 
+        if (click == BlockFace.UP)
+            return true;
+
+        boolean left, right;
+
+        switch (s.getShape()) {
+            case OUTER_LEFT:
+            case OUTER_RIGHT:
+                return true;
+            case INNER_LEFT:
+                left = true;
+                right = false;
+                break;
+            case INNER_RIGHT:
+                left = false;
+                right = true;
+                break;
+            case STRAIGHT:
+                left = false;
+                right = false;
+                break;
+            default:
+                return false;
+        }
+
+        switch (s.getFacing()) {
+            case EAST:
+                return click == BlockFace.WEST
+                        || left && click == BlockFace.SOUTH
+                        || right && click == BlockFace.NORTH
+                ;
+            case SOUTH:
+                return click == BlockFace.NORTH
+                        || left && click == BlockFace.WEST
+                        || right && click == BlockFace.EAST
+                ;
+            case WEST:
+                return click == BlockFace.EAST
+                        || left && click == BlockFace.NORTH
+                        || right && click == BlockFace.SOUTH
+                ;
+            case NORTH:
+                return click == BlockFace.SOUTH
+                        || left && click == BlockFace.EAST
+                        || right && click == BlockFace.WEST
+                ;
+            default:
+                return false;
+        }
+    }
+
+    private void chairMount(Player p, Block b, Stairs s) {
         float yaw;
         switch (s.getFacing()) {
             case EAST:
@@ -124,7 +189,7 @@ public class ChairListener implements Listener {
 
         mounted.put(e, b);
         Location to = p.getLocation();
-        to.setYaw(to.getYaw() + 180);
+        to.setYaw(yaw);
         p.teleport(to, PlayerTeleportEvent.TeleportCause.PLUGIN);
         e.addPassenger(p);
     }
@@ -133,64 +198,76 @@ public class ChairListener implements Listener {
         if (armorStand instanceof ArmorStand) {
             Block b = mounted.remove(armorStand);
             if (b != null) {
+
                 Stairs s = (Stairs) b.getBlockData();
-                double x, z;
-                boolean left, right;
+                double x, y, z;
 
-                switch (s.getShape()) {
-                    case INNER_LEFT:
-                    case OUTER_LEFT:
-                        left = true;
-                        right = false;
-                        break;
-                    case INNER_RIGHT:
-                    case OUTER_RIGHT:
-                        right = true;
-                        left = false;
-                        break;
-                    default:
-                        left = false;
-                        right = false;
+                Block facingLow = b.getRelative(s.getFacing().getOppositeFace());
+                Block facingHigh = facingLow.getRelative(BlockFace.UP);
+
+
+
+                if (facingHigh.isPassable()) {
+                    boolean left, right;
+                    switch (s.getShape()) {
+                        case INNER_LEFT:
+                        case OUTER_LEFT:
+                            left = true;
+                            right = false;
+                            break;
+                        case INNER_RIGHT:
+                        case OUTER_RIGHT:
+                            right = true;
+                            left = false;
+                            break;
+                        default:
+                            left = false;
+                            right = false;
+                    }
+
+                    switch (s.getFacing()) {
+                        case NORTH:
+                            x = left
+                                    ? 1.0
+                                    : right
+                                    ? 0.0
+                                    : 0.5;
+                            z = 1.0;
+                            break;
+                        case WEST:
+                            x = 1.0;
+                            z = left
+                                    ? 0.0
+                                    : right
+                                    ? 1.0
+                                    : 0.5;
+                            break;
+                        case SOUTH:
+                            x = left
+                                    ? 0.0
+                                    : right
+                                    ? 1.0
+                                    : 0.5;
+                            z = 0.0;
+                            break;
+                        case EAST:
+                            x = 0.0;
+                            z = left
+                                    ? 1.0
+                                    : right
+                                    ? 0.0
+                                    : 0.5;
+                            break;
+                        default:
+                            x = z = 0.5;
+                    }
+                    y = facingLow.isPassable() ? 0.5 : 1.0;
+                } else {
+                    x = z = 0.5;
+                    y = 1.0;
                 }
 
-                switch (s.getFacing()) {
-                    case NORTH:
-                        x = left
-                                ? 1.0
-                                : right
-                                ? 0.0
-                                : 0.5;
-                        z = 1.0;
-                        break;
-                    case WEST:
-                        x = 1.0;
-                        z = left
-                                ? 1.0
-                                : right
-                                ? 0.0
-                                : 0.5;
-                        break;
-                    case SOUTH:
-                        x = left
-                                ? 0.0
-                                : right
-                                ? 1.0
-                                : 0.5;
-                        z = 0.0;
-                        break;
-                    case EAST:
-                        x = 0.0;
-                        z = left
-                                ? 0.0
-                                : right
-                                ? 1.0
-                                : 0.5;
-                        break;
-                    default:
-                        x = z = 0.5;
-                }
-
-                ejectAll(armorStand, b.getLocation().add(x, 0.0, z), tpImmediately);
+                ejectAll(armorStand, b.getLocation().add(x, y, z), tpImmediately);
                 armorStand.remove();
             }
         }
@@ -198,15 +275,18 @@ public class ChairListener implements Listener {
 
     private void ejectAll(Entity armorStand, Location loc, boolean tpImmediately) {
         for (Entity e : armorStand.getPassengers()) {
-            e.leaveVehicle();
             Location to = loc.clone();
             Location el = e.getLocation();
             to.setYaw(el.getYaw());
             to.setPitch(el.getPitch());
-            if (tpImmediately) {
+            if (tpImmediately || !(e instanceof Player)) {
+                e.leaveVehicle();
                 e.teleport(to, PlayerTeleportEvent.TeleportCause.UNKNOWN);
             } else {
-                postDismountTeleport.put(e, to);
+                Player p = (Player) e;
+                p.setNoDamageTicks(10);
+                postDismountTeleport.put(p, to);
+                e.leaveVehicle();
             }
         }
     }
